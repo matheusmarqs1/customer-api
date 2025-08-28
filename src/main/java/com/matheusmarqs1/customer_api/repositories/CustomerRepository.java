59957467e878632d00d5775f91e5cb1eb9b98e8a
Matheus Teles <matheusti770@gmail.com>
2025-08-28 17:52:59 -0300

    Create CustomerService with custom exceptions (ResourceNotFoundException, BusinessException)

--- src/main/java/com/matheusmarqs1/customer_api/repositories/CustomerRepository.java
+++ src/main/java/com/matheusmarqs1/customer_api/repositories/CustomerRepository.java
@@ -1,9 +1,12 @@
 package com.matheusmarqs1.customer_api.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.matheusmarqs1.customer_api.entities.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
+	Optional<Customer> findByCpf(String cpf);
+	Optional<Customer> findByEmail(String email);
 }
---
+++ src/main/java/com/matheusmarqs1/customer_api/services/CustomerService.java
@@ -0,0 +1,85 @@
+package com.matheusmarqs1.customer_api.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.matheusmarqs1.customer_api.dtos.customer.CustomerCreateRequest;
+import com.matheusmarqs1.customer_api.dtos.customer.CustomerResponse;
+import com.matheusmarqs1.customer_api.dtos.customer.CustomerUpdateRequest;
+import com.matheusmarqs1.customer_api.entities.Customer;
+import com.matheusmarqs1.customer_api.repositories.CustomerRepository;
+import com.matheusmarqs1.customer_api.services.exceptions.BusinessException;
+import com.matheusmarqs1.customer_api.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class CustomerService {
+	
+	private final CustomerRepository customerRepository;
+	
+	public CustomerService(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+	
+	public List<CustomerResponse> getAllCustomers(){
+		return customerRepository.findAll().stream()
+				.map(CustomerResponse::fromEntity)
+				.toList();
+	}
+	
+	public CustomerResponse getCustomerById(Long id) {
+		Customer customer = customerRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Customer not found with ID: " + id));
+		
+		return CustomerResponse.fromEntity(customer);
+	}
+	
+	public CustomerResponse createCustomer(CustomerCreateRequest createRequest) {
+		if(customerRepository.findByCpf(createRequest.cpf()).isPresent()) {
+			throw new BusinessException("CPF already exists");
+		}
+		
+		if(customerRepository.findByEmail(createRequest.email()).isPresent()) {
+			throw new BusinessException("Email already exists");
+		}
+		
+		Customer customer = new Customer(null, 
+				createRequest.name(),
+				createRequest.cpf(),
+				createRequest.email(),
+				createRequest.birthDate(),
+				createRequest.phone());
+		
+		Customer savedCustomer = customerRepository.save(customer);
+		return CustomerResponse.fromEntity(savedCustomer);
+		
+	}
+	
+	public CustomerResponse updateCustomer(Long id, CustomerUpdateRequest updateRequest) {
+		Customer customer = customerRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Customer not found with ID: " + id));
+		
+		customerRepository.findByEmail(updateRequest.email()).ifPresent(existingCustomer -> {
+			if(!existingCustomer.getId().equals(id)) {
+				throw new BusinessException("Email already exists");
+			}
+		});
+		
+		customer.setName(updateRequest.name());
+		customer.setEmail(updateRequest.email());
+		customer.setBirthDate(updateRequest.birthDate());
+		customer.setPhone(updateRequest.phone());
+		
+		Customer savedCustomer = customerRepository.save(customer);
+		return CustomerResponse.fromEntity(savedCustomer);
+		
+	}
+	
+	public void deleteCustomer(Long id) {
+		if(!customerRepository.existsById(id)) {
+			throw new ResourceNotFoundException("Customer not found with ID: " + id);
+		}
+		
+		customerRepository.deleteById(id);
+	}
+}
